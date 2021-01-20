79eae74afebf7ccde3944b250f4046e8fc7253a3
mullena14 <alicia.s.mullen@gmail.com>
2021-01-19 19:18:24 -0800

    Removed my-site.html and all references to it.

---
+++ my-site.js
@@ -0,0 +1,62 @@
+"use strict";
+(function() {
+  window.addEventListener("load", init);
+
+  function init() {
+
+  }
+
+  /**
+ * Make sure to always add a descriptive comment above
+ * every function detailing what it's purpose is
+ * @param {variabletype} someVariable This is a description of someVariable, including, perhaps, preconditions.
+ * @returns {returntype} A description of what this function is actually returning
+ */
+function exampleFunction2(someVariable) {
+  /* SOME CODE */
+  return something;
+}
+
+  function home() {
+    window.location = "https://mullena14.github.io/Personal_Site/";
+  }
+
+  function home() {
+    window.location = "https://mullena14.github.io/Personal_Site/engineer.html";
+  }
+
+  function home() {
+    window.location = "https://mullena14.github.io/Personal_Site/";
+  }
+
+  function home() {
+    window.location = "https://mullena14.github.io/Personal_Site/";
+  }
+
+  function home() {
+    window.location = "https://mullena14.github.io/Personal_Site/";
+  }
+
+  function home() {
+    window.location = "https://mullena14.github.io/Personal_Site/";
+  }
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} selector - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qsa(selector) {
+    return document.querySelectorAll(selector);
+  }
+
+})();
